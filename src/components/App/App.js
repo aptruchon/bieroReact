b76f831e0b4118642514a90147556f921ba973f3
aptruchon <aptruchon@gmail.com>
2022-11-07 14:09:48 -0500

    entete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,14 @@
+import React from 'react';
+import Entete from '../Entete/Entete';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  render() {
+    return (
+      <>
+        <Entete />
+				<p>dfkjsdfh</p>
+			</>
+		);
+	}
 }
-
-export default App;
---
+++ src/components/Entete/Entete.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Entete.css';
+
+export default class Entete extends React.Component{
+  render() {
+    return (
+      <p>Je suis une entete</p>
+    );
+  }
+}
