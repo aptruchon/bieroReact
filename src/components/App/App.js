b432d96923c32bf8aa742af26f05f67f52f4e6da
aptruchon <aptruchon@gmail.com>
2022-11-08 14:43:01 -0500

    fin incrementation niveau supérieur

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,29 @@ import Entete from '../Entete/Entete';
 import './App.css';
 
 export default class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      valeur : 0
+    }
+  }
+
+
+  incremente = () =>{
+    // console.log("increment");
+
+    // Même chose mais différent et il y a une raison de pk on utlise l'autre
+    //this.setState({ valeur : ++this.state.valeur});
+    this.setState((state) => ({valeur : this.state.valeur + 1}))
+  }
+
+  // <Compteur valeurInitiale="0"/>
   render() {
     return (
       <>
         <Entete titre="Biero" test="valeur"/>
-        <BoutonNav titre="BoutonNav" test="valeur"/>
-        <Compteur valeurInitiale="0" />
+        <Compteur handleIncrement={this.incremente} valeur={this.state.valeur}/>
 			</>
 		);
 	}
--- src/components/Compteur/Compteur.js
+++ src/components/Compteur/Compteur.js
@@ -6,25 +6,13 @@ export default class Compteur extends React.Component{
     constructor(props){
         super(props);
         // this.incremente = this.incremente.bind(this);
-
-        this.state = {
-            valeur : parseInt(this.props.valeurInitiale) || 0
-        }
-    }
-
-    incremente = () =>{
-        // console.log("increment");
-
-        this.setState({ valeur : ++this.state.valeur});
-        // Même chose plus complexe
-        // this.setState((state) => ({valeur : this.state.valeur + 1}))
     }
 
     render() {
         return (
             <div>
-                <p>{this.state.valeur}</p>
-                <button onClick={this.incremente}>Incrémente</button>
+                <p>{this.props.valeur}</p>
+                <button onClick={this.props.handleIncrement}>Incrémente</button>
             </div>
         );
     }
